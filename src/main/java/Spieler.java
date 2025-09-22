745a913ab1bcf9cc303057ad67d6d046c0ab8140
BPfirsich <bennionline05@gmail.com>
2025-09-22 11:14:37 +0200

    added Class Spieler

---
+++ src/main/java/Spieler.java
@@ -0,0 +1,29 @@
+class Spieler {
+    Rectangle figur;
+    double ySpeed = 0;
+    boolean amBoden = true;
+
+    public Spieler(double x, double y) {
+        figur = new Rectangle(30, 40, Color.BLUE);
+        figur.setTranslateX(x);
+        figur.setTranslateY(y);
+    }
+
+    public void springen() {
+        if (amBoden) {
+            ySpeed = -12;      // Sprungkraft
+            amBoden = false;
+        }
+    }
+
+    public void update() {
+        ySpeed += 0.5; // Schwerkraft
+        figur.setTranslateY(figur.getTranslateY() + ySpeed);
+
+        if (figur.getTranslateY() >= 360) { // Boden bei y=360
+            figur.setTranslateY(360);
+            amBoden = true;
+            ySpeed = 0;
+        }
+    }
+}
