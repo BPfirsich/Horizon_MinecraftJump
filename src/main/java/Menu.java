dc8917afcf9a07d977583bd7f45c3a763c3a84ff
BPfirsich <bennionline05@gmail.com>
2025-11-12 18:09:17 +0100

    Made all Buttons invisible

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -34,6 +34,7 @@ public class Menu {
         startGameButton.setLayoutX(463);
         startGameButton.setLayoutY(310);
         startGameButton.setOnAction(event -> {loadLevel1.apply(null);});
+        startGameButton.setOpacity(0);
         root.getChildren().add(startGameButton);
 
         Button changeLvlButton = new Button();
@@ -42,6 +43,7 @@ public class Menu {
         changeLvlButton.setLayoutX(463);
         changeLvlButton.setLayoutY(405);
         changeLvlButton.setOnAction(event -> {toLevelScreen.apply(null);});
+        changeLvlButton.setOpacity(0);
         root.getChildren().add(changeLvlButton);
 
         Button endBt = new Button();
@@ -50,6 +52,7 @@ public class Menu {
         endBt.setLayoutX(1010);
         endBt.setLayoutY(530);
         endBt.setOnAction(event -> {System.exit(0);});
+        endBt.setOpacity(0);
         root.getChildren().add(endBt);
 
         return newScene;
@@ -75,6 +78,7 @@ public class Menu {
         backBt.setLayoutX(145);
         backBt.setLayoutY(220);
         backBt.setOnAction(event -> {backFunction.apply(null);});
+        backBt.setOpacity(0);
         root.getChildren().add(backBt);
 
         Button o1Button = new Button();
@@ -83,6 +87,7 @@ public class Menu {
         o1Button.setLayoutX(463);
         o1Button.setLayoutY(405);
         o1Button.setOnAction(event -> {loadFunction.apply("o1");});
+        o1Button.setOpacity(0);
         root.getChildren().add(o1Button);
 
         Button o2Button = new Button();
@@ -91,6 +96,7 @@ public class Menu {
         o2Button.setLayoutX(463);
         o2Button.setLayoutY(495);
         o2Button.setOnAction(event -> {loadFunction.apply("o2");});
+        o2Button.setOpacity(0);
         root.getChildren().add(o2Button);
 
         Button o3Button = new Button();
@@ -99,6 +105,7 @@ public class Menu {
         o3Button.setLayoutX(463);
         o3Button.setLayoutY(585);
         o3Button.setOnAction(event -> {loadFunction.apply("o3");});
+        o3Button.setOpacity(0);
         root.getChildren().add(o3Button);
 
         Button n1Button = new Button();
@@ -107,6 +114,7 @@ public class Menu {
         n1Button.setLayoutX(720);
         n1Button.setLayoutY(405);
         n1Button.setOnAction(event -> {loadFunction.apply("n1");});
+        n1Button.setOpacity(0);
         root.getChildren().add(n1Button);
 
         Button n2Button = new Button();
@@ -115,6 +123,7 @@ public class Menu {
         n2Button.setLayoutX(720);
         n2Button.setLayoutY(495);
         n2Button.setOnAction(event -> {loadFunction.apply("n2");});
+        n2Button.setOpacity(0);
         root.getChildren().add(n2Button);
 
         Button n3Button = new Button();
@@ -123,7 +132,35 @@ public class Menu {
         n3Button.setLayoutX(720);
         n3Button.setLayoutY(585);
         n3Button.setOnAction(event -> {loadFunction.apply("n3");});
+        n3Button.setOpacity(0);
         root.getChildren().add(n3Button);
+
+        Button e1Button = new Button();
+        e1Button.setPrefWidth(240);
+        e1Button.setPrefHeight(80);
+        e1Button.setLayoutX(980);
+        e1Button.setLayoutY(405);
+        e1Button.setOnAction(event -> {loadFunction.apply("e1");});
+        e1Button.setOpacity(0);
+        root.getChildren().add(e1Button);
+
+        Button e2Button = new Button();
+        e2Button.setPrefWidth(240);
+        e2Button.setPrefHeight(80);
+        e2Button.setLayoutX(980);
+        e2Button.setLayoutY(495);
+        e2Button.setOnAction(event -> {loadFunction.apply("e2");});
+        e2Button.setOpacity(0);
+        root.getChildren().add(e2Button);
+
+        Button e3Button = new Button();
+        e3Button.setPrefWidth(240);
+        e3Button.setPrefHeight(80);
+        e3Button.setLayoutX(980);
+        e3Button.setLayoutY(585);
+        e3Button.setOnAction(event -> {loadFunction.apply("e3");});
+        e3Button.setOpacity(0);
+        root.getChildren().add(e3Button);
 
         return newScene;
     }
